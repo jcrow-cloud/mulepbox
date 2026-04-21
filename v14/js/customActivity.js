8832bfd53281311355add81cb84b110fe13334ff
jcrow-cloud <jeannette.crow@perficient.com>
2026-04-21 16:00:46 -0600

    Create customActivity.js

---
+++ v14/js/customActivity.js
@@ -0,0 +1,110 @@
+/* global Postmonger */
+(function () {
+  'use strict';
+
+  var connection = new Postmonger.Session();
+  var activity = {};
+
+  document.addEventListener('DOMContentLoaded', function () {
+    connection.on('initActivity', onInitActivity);
+    connection.on('clickedNext', onDone);
+    connection.trigger('ready');
+  });
+
+  function onInitActivity(payload) {
+    activity = payload || {};
+
+    var data =
+      activity.arguments &&
+      activity.arguments.execute &&
+      activity.arguments.execute.inArguments &&
+      activity.arguments.execute.inArguments[0] &&
+      activity.arguments.execute.inArguments[0].data
+        ? activity.arguments.execute.inArguments[0].data
+        : {};
+
+    var entryInput = document.getElementById('apiJourneyEntryName');
+    var templateInput = document.getElementById('templateName');
+    var subjectInput = document.getElementById('subject');
+
+    if (entryInput) entryInput.value = data.apiJourneyEntryName || '';
+    if (templateInput) templateInput.value = data.template_name || '';
+    if (subjectInput) subjectInput.value = data.subject || '';
+  }
+
+  function onDone() {
+    activity.arguments = activity.arguments || {};
+    activity.arguments.execute = activity.arguments.execute || {};
+
+    var entryKey = document.getElementById('apiJourneyEntryName')?.value || '';
+    var templateName = document.getElementById('templateName')?.value || '';
+    var subject = document.getElementById('subject')?.value || '';
+
+    // Helper to build {{Event.<key>.<field>}}
+    function ev(field) {
+      return '{{Event.' + entryKey + '.' + field + '}}';
+    }
+
+    var templateValues = {
+      confirmationCode: ev('confirmationCode'),
+      guestId: ev('guestId'),
+      prsProfileId: ev('prsProfileId'),
+      firstName: ev('firstName'),
+      email: ev('email'),
+      phoneNumber: ev('phoneNumber'),
+      address_1: ev('address_1'),
+      address_2: ev('address_2'),
+      city: ev('city'),
+      zip: ev('zip'),
+      state: ev('state'),
+      emailPrereregisterUrl: ev('emailPrereregisterUrl'),
+      smsPrereregisterUrl: ev('smsPrereregisterUrl'),
+      appointmentStatus: ev('appointmentStatus'),
+      appointmentType: ev('appointmentType'),
+      appointmentDateTime: ev('appointmentDateTime'),
+      appointmentTimeZone: ev('appointmentTimeZone'),
+      appointmentEmailOptin: ev('appointmentEmailOptin'),
+      appointmentSMSOptin: ev('appointmentSMSOptin'),
+      apptDeepLink: ev('apptDeepLink'),
+      apptDeepLinkForModify: ev('apptDeepLinkForModify'),
+      apptDeepLinkForCancel: ev('apptDeepLinkForCancel'),
+      paymentOOP: ev('paymentOOP'),
+      reasonForVisit: ev('reasonForVisit'),
+      siteCode: ev('siteCode'),
+      partnerName: ev('partnerName'),
+      eorderAppointment: ev('eorderAppointment'),
+      confirmAppointmentURL: ev('confirmAppointmentURL'),
+      outstandingBalance: ev('outstandingBalance'),
+      calendarLink: ev('calendarLink')
+    };
+
+    activity.arguments.execute.inArguments = [
+      {
+        data: {
+          apiJourneyEntryName: entryKey,
+          template_name: templateName,
+          template_values: JSON.stringify(templateValues),
+          message: {
+            recipients: [ev('email')],
+            headers: {
+              subject: subject,
+              from: 'noreply@pcomm.questdiagnostics.com'
+            },
+            allowNonTLS: false,
+            forceSecureNotification: false
+          }
+        }
+      }
+    ];
+
+    activity.arguments.execute.outArguments = [
+      { status: 'DefaultStatus' }
+    ];
+
+    activity.metaData = activity.metaData || {};
+    activity.metaData.isConfigured = true;
+
+    connection.trigger('updateActivity', activity);
+  }
+
+})();
