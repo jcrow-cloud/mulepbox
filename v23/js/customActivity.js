72ca5b97e2406c022635d49f5b2e66ce57de67bf
jcrow-cloud <jeannette.crow@perficient.com>
2026-05-08 11:55:19 -0600

    Create customActivity.js

---
+++ v23/js/customActivity.js
@@ -0,0 +1,72 @@
+/* global Postmonger */
+(function () {
+  'use strict';
+
+  var connection = new Postmonger.Session();
+  var activity = {};
+  var schema = [];
+
+  document.addEventListener('DOMContentLoaded', function () {
+    connection.on('initActivity', onInitActivity);
+    connection.on('requestedSchema', onRequestedSchema);
+    connection.on('clickedNext', onDone);
+
+    connection.trigger('ready');
+    connection.trigger('requestSchema');
+  });
+
+  function onInitActivity(payload) {
+    activity = payload || {};
+
+    var args =
+      activity.arguments &&
+      activity.arguments.execute &&
+      activity.arguments.execute.inArguments &&
+      activity.arguments.execute.inArguments[0]
+        ? activity.arguments.execute.inArguments[0]
+        : {};
+
+    var templateInput = document.getElementById('templateName');
+    var subjectInput = document.getElementById('subject');
+
+    if (templateInput) {
+      templateInput.value = args.templateName || '';
+    }
+
+    if (subjectInput) {
+      subjectInput.value = args.subject || '';
+    }
+  }
+
+  function onRequestedSchema(payload) {
+    schema = payload && payload.schema ? payload.schema : [];
+  }
+
+  function onDone() {
+    activity.arguments = activity.arguments || {};
+    activity.arguments.execute = activity.arguments.execute || {};
+
+    var templateInput = document.getElementById('templateName');
+    var subjectInput = document.getElementById('subject');
+
+    var templateName = templateInput ? templateInput.value : '';
+    var subject = subjectInput ? subjectInput.value : '';
+
+    activity.arguments.execute.inArguments = [
+      {
+        templateName: templateName,
+        subject: subject
+      }
+    ];
+
+    activity.arguments.execute.outArguments = [
+      { status: 'DefaultStatus' }
+    ];
+
+    activity.metaData = activity.metaData || {};
+    activity.metaData.isConfigured = true;
+
+    connection.trigger('updateActivity', activity);
+  }
+
+})();
